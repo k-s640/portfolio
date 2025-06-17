57f18802c5f4a3836323307d4605a698a25a5ffb
k-s640 <kamimura-s2405@school.ac.jp>
2025-06-17 13:42:45 +0900

    Add files via upload

--- Bs5_kadai_01_starter_b/main.js
+++ Bs5_kadai_01_starter_b/main.js
@@ -1,1 +1,16 @@
 console.log("main.js!!");
+document.addEventListener("DOMContentLoaded", function () {
+    const buttonA = document.querySelector(".btn-primary"); // ボタンA
+    const buttonB = document.querySelector(".btn-light"); // ボタンB
+    const content = document.querySelector("#content"); // 変更対象の要素
+
+    buttonA.addEventListener("click", function () {
+        content.style.backgroundColor = "white";  // 背景を白色に
+        content.style.color = "inherit";  // テキストを黒色に
+    });
+
+    buttonB.addEventListener("click", function () {
+        content.style.backgroundColor = "#212529";  // 背景を黒色に
+        content.style.color = "white";  // テキストを白色に
+    });
+});
